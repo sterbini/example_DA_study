494ae47f1683727cd900cb30751cb819609e38eb
andrew960 <andrew960@hotmail.it>
2023-07-19 19:29:14 +0200

    new study_dispersion

---
+++ master_study/master_jobs/2_configure_and_track/study_dispersion.py
@@ -0,0 +1,105 @@
+# %%
+import xtrack as xt
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+# %%
+# %%
+collider = xt.Multiline.from_json('collider.json')
+
+# %%
+collider.build_trackers()
+
+# %%
+
+def set_orbit_flat(collider):
+    print('Setting optics as flat')
+    for ii in ['on_x1', 'on_sep1', 'on_x2h', 'on_sep2h', 'on_x2v', 'on_sep2v', 'on_x5', 
+               'on_sep5', 'on_x8h', 'on_sep8h', 'on_x8v', 'on_sep8v', 'on_disp', 
+               'on_alice_normalized', 'on_lhcb_normalized','on_sol_atlas', 'on_sol_cms', 
+               'on_sol_alice', 'i_oct_b1', 'i_oct_b2']:
+        collider.vars[ii] = 0
+
+def set_orbit_from_config(collider, config):
+    print('Setting optics as from config')
+    for ii in ['on_x1', 'on_sep1', 'on_x2h', 'on_sep2h', 'on_x2v', 'on_sep2v', 'on_x5', 
+               'on_sep5', 'on_x8h', 'on_sep8h', 'on_x8v', 'on_sep8v', 'on_disp', 
+               'on_alice_normalized', 'on_lhcb_normalized', 'on_sol_atlas', 'on_sol_cms', 
+               'on_sol_alice', 'i_oct_b1', 'i_oct_b2']:
+        collider.vars[ii] = config['config_collider']['config_knobs_and_tuning']['knob_settings'][ii]
+# %%
+#First problem: the x closed orbit starts at zero and then an oscillation begins at IP8 and
+#the y closed orbit is oscillating until IP2
+collider.vars['beambeam_scale'] = 0
+collider.vars['vrf400'] = 12
+set_orbit_flat(collider)
+twiss_b1 = collider['lhcb1'].twiss()
+twiss_b2 = collider['lhcb2'].twiss().reverse()
+
+# %%
+plt.plot(twiss_b1['s', : ], twiss_b1['x', : ],label='x')
+plt.plot(twiss_b2['s', : ], twiss_b2['y', : ],label='y')
+plt.axvline(twiss_b2[['s'],'ip8'],color = 'green', linestyle='-.', label='IP8')
+plt.axvline(twiss_b2[['s'],'ip1'],color = 'black', linestyle='-.', label='IP1')
+plt.axvline(twiss_b2[['s'],'ip2'],color = 'red', linestyle='-.', label='IP2')
+plt.title(f'Closed orbit')
+plt.ylim(-2e-17,2e-17)
+plt.legend()
+plt.grid(True)
+# %%
+# %%
+#Second problem: y dispersion
+collider.vars['beambeam_scale'] = 0
+collider.vars['vrf400'] = 12
+set_orbit_flat(collider)
+twiss_b1 = collider['lhcb1'].twiss()
+twiss_b2 = collider['lhcb2'].twiss().reverse()
+
+# %%
+#plt.plot(twiss_b1['s', : ], twiss_b1['dx', : ],label='x')
+plt.plot(twiss_b2['s', : ], twiss_b2['dy', : ],label='y')
+plt.axvline(twiss_b2[['s'],'ip8'],color = 'green', linestyle='-.', label='IP8',alpha = 0.5)
+plt.axvline(twiss_b2[['s'],'ip1'],color = 'black', linestyle='-.', label='IP1',alpha = 0.5)
+plt.axvline(twiss_b2[['s'],'ip2'],color = 'red', linestyle='-.', label='IP2',alpha = 0.5)
+plt.title(f'Dispersion')
+plt.legend()
+plt.grid(True)
+# %%
+#We can study the rotated lattice
+collider_start = xt.Multiline.from_json('/afs/cern.ch/work/a/afornara/public/run3/example_DA_study/master_study/master_jobs/1_build_distr_and_collider/collider/collider.json')
+
+# %%
+collider_start.build_trackers()
+
+# %%
+#Here the orbit is zero!
+set_orbit_flat(collider_start)
+# collider.vars['beambeam_scale'] = 0 
+twiss_b1_start = collider_start['lhcb1'].twiss()
+twiss_b2_start = collider_start['lhcb2'].twiss().reverse()
+
+plt.plot(twiss_b1_start['s', : ], twiss_b1_start['x', : ],label='x')
+plt.plot(twiss_b2_start['s', : ], twiss_b2_start['y', : ],label='y')
+plt.axvline(twiss_b2_start[['s'],'ip8'],color = 'green', linestyle='-.', label='IP8')
+plt.axvline(twiss_b2_start[['s'],'ip1'],color = 'black', linestyle='-.', label='IP1')
+plt.axvline(twiss_b2_start[['s'],'ip2'],color = 'red', linestyle='-.', label='IP2')
+plt.title(f'Closed orbit')
+plt.ylim(-2e-17,2e-17)
+plt.legend()
+plt.grid(True)
+# %%
+
+set_orbit_flat(collider_start)
+# collider.vars['beambeam_scale'] = 0 
+twiss_b1_start = collider_start['lhcb1'].twiss()
+twiss_b2_start = collider_start['lhcb2'].twiss().reverse()
+
+#plt.plot(twiss_b1_start['s', : ], twiss_b1_start['dx', : ],label='x')
+plt.plot(twiss_b2_start['s', : ], twiss_b2_start['dy', : ],label='y')
+plt.axvline(twiss_b2_start[['s'],'ip8'],color = 'green', linestyle='-.', label='IP8')
+plt.axvline(twiss_b2_start[['s'],'ip1'],color = 'black', linestyle='-.', label='IP1')
+plt.axvline(twiss_b2_start[['s'],'ip2'],color = 'red', linestyle='-.', label='IP2')
+plt.title(f'Dispersion')
+plt.legend()
+plt.grid(True)
+# %%
